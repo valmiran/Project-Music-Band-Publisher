fa085430e92663098a3fcdcfb96b3951a49c739a
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2025-09-23 13:58:34 -0300

    Completing the migration and adjusting the apps

--- music_band/settings.py
+++ music_band/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'music_band.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'music_band_db',
+        'NAME': 'musicbanddb',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
