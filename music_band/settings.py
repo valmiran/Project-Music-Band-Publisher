a45d202ef266c40995a8b3b6e2beb4a80e7cfcba
pedro vinicius <pedrovinipv58@gmail.com>
2025-11-30 22:54:22 -0300

    [Auth] Recuperação de Senha concluído

--- music_band/settings.py
+++ music_band/settings.py
@@ -149,3 +149,15 @@ DATETIME_INPUT_FORMATS = [
     '%d/%m/%Y %H:%M:%S',     # Padrão brasileiro
     '%d/%m/%Y %H:%M',        # Padrão brasileiro curto
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'seu.email@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha-de-app-do-google'
+'''
